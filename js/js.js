0bc80d464ae5f4f4905ca61d93654ddedd0021c7
Angelo Rocha <angelo.ofp@gmail.com>
2013-06-24 15:46:34 -0300

    Changes made in the layout structure, read more for details.
    centerzone finalized and restructured;
    blocks restructured;
    Added ARIA HTML5 around the theme;
    navMenu relocated;
    adjustments in template news;
    added tooltip js icons in the news;
    several adjustments in blocks mainmenu and usermenu;

--- js/js.js
+++ js/js.js
@@ -1,1 +1,13 @@
-// JavaScript Document
+$(document).ready(function() {
+    $('.news-tools a').tooltip( //altere para a classe desejada
+		$.fn.tooltip.defaults = {
+		animation: true
+		, placement: 'top'   // Altera para a direção desejada
+		, selector: false
+		, template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+		, trigger: 'hover'
+		, title: ''
+		, delay: 0
+		, html: false
+		})
+})
