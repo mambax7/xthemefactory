fe88120960bd123f99dd4b1e2d8ad86215cccb23
Angelo Rocha <angelo.ofp@gmail.com>
2013-06-26 14:02:10 -0300

    added some files

--- js/js.js
+++ js/js.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-    $('.news-tools a').tooltip( //altere para a classe desejada
-		$.fn.tooltip.defaults = {
+// JavaScript Document
+var TF = $.noConflict();
+TF(document).ready(function() {
+    TF('.news-tools a, .tagbar a').tooltip(
+		TF.fn.tooltip.defaults = {
 		animation: true
-		, placement: 'top'   // Altera para a direção desejada
+		, placement: 'top'
 		, selector: false
 		, template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
 		, trigger: 'hover'
 		, title: ''
 		, delay: 0
 		, html: false
-		})
+	})
 })
