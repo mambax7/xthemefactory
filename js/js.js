efb1f25295b96a5edb1c900e652ecabb2d41c59a
Angelo Rocha <angelo.ofp@gmail.com>
2013-07-17 15:05:32 -0300

    Restructuring of the project
    - Restructuring of the project;
    - Added bootstrap slide;
    - Css many adjustments;

--- js/js.js
+++ js/js.js
@@ -1,15 +1,13 @@
-// JavaScript Document
-var TF = $.noConflict();
-TF(document).ready(function() {
-    TF('.news-tools a, .tagbar a').tooltip(
-		TF.fn.tooltip.defaults = {
+$(document).ready(function() {
+    $('.news-tools a').tooltip( //altere para a classe desejada
+		$.fn.tooltip.defaults = {
 		animation: true
-		, placement: 'top'
+		, placement: 'top'   // Altera para a direção desejada
 		, selector: false
 		, template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
 		, trigger: 'hover'
 		, title: ''
 		, delay: 0
 		, html: false
-	})
+		})
 })
